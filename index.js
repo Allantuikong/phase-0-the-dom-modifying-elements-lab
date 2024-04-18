0d0c135c53153a68b5d7554983a06da645b6dbfe
Allan Kipkorir <allantuix@gmail.com>
2024-04-18 14:59:54 +0300

    Practiced DOM Manipulation

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+const mainNode = document.querySelector("#main")
+
+mainNode.remove()
+
+const newHeader = document.createElement("h1")
+
+newHeader.id = "victory"
+
+newHeader.textContent = "JESUS is the champion"
+
+document.body.append(newHeader)
+
